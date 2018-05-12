92a89bf32e9e60823a4f7fb55c2bf7b919ba11b7
lipnus <sunpil13@korea.ac.kr>
2018-05-12 13:06:39 +0900

    추천

--- src/app/model/sound-recommend.ts
+++ src/app/model/sound-recommend.ts
@@ -10,4 +10,14 @@ export class SoundRecommend{
   mood2:string;
   mood3:string;
 
+  constructor(){
+    this.recommend_pk=0;
+    this.bpm=0;
+    this.genre1="";
+    this.genre2="";
+    this.mood1="";
+    this.mood2="";
+    this.mood3="";
+  }
+
 }
--- src/app/recommend/recommend.component.ts
+++ src/app/recommend/recommend.component.ts
@@ -21,6 +21,7 @@ export class RecommendComponent implements OnInit {
 
   recCount:number;
   youtubePath;
+  soundRecommend:SoundRecommend; //추천데이터 정보
   aaa;
 
   constructor(private postToServerService: PostToServerService,
@@ -30,24 +31,42 @@ export class RecommendComponent implements OnInit {
 
   ngOnInit() {
     this.recCount=1;
-    this.youtubePath = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/VSZ4HwlUSXU');
+    this.postRecommend("request", 0);
   }
 
 
 
-  postRecommend(){
+  setYoutubePath(youtubePath:string){
+    let pathArray = youtubePath.split("=");
+    this.youtubePath = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + pathArray[pathArray.length-1]);
+  }
+
+
+
+  postRecommend(type:string, score:number){
     var path = "/recommend";
-    var postData = {token:0, type:"request"};
+    var postData = {
+      token:0,
+      type:type,
+      // recommend_pk:this.soundRecommend.recommend_pk,
+      // bpm:this.soundRecommend.bpm,
+      // genre1:this.soundRecommend.genre1,
+      // genre2:this.soundRecommend.genre2,
+      // mood1:this.soundRecommend.mood1,
+      // mood2:this.soundRecommend.mood2,
+      // mood3:this.soundRecommend.mood3,
+      // score:score
+    };
+
     this.postToServerService.postServer(path, postData).subscribe(data => {
       console.log(data.youtube);
-      this.youtubePath = data.youtube;
+      this.soundRecommend = data;
+      this.setYoutubePath(data.youtube);
       this.recCount++;
     });
   }
 
   onClick_score(score:number){
-    this.postRecommend();
+    this.postRecommend("answwer", score);
   }
-
-
 }
