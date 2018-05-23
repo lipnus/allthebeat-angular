55fbad1ee84e6958f21f781246c662e1defc19f9
lipnus <sunpil13@korea.ac.kr>
2018-05-23 14:12:08 +0900

    추천데탑

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -63,7 +63,7 @@ export class MainpageComponent implements OnInit {
     this.soundCount=0;
     this.recommendTitle = "랜덤추천비트";
 
-    // localStorage.setItem('auth', JSON.stringify({ token: "AAAAONndpSvLnYPdPKQH2VMDkpuDthI00hmRLsPH+LWvdfxOJHyAIsonS33Zm5W4NGbCL2hB3ZAp4qYyqS1S8nyZL2U=" }));
+    localStorage.setItem('auth', JSON.stringify({ token: "AAAAONndpSvLnYPdPKQH2VMDkpuDthI00hmRLsPH+LWvdfxOJHyAIsonS33Zm5W4NGbCL2hB3ZAp4qYyqS1S8nyZL2U=" }));
 
     this.postSoundList();
 
